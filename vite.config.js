34efa56e71426d4d9fc069096d2c01f19b6979a4
Timofei Sukhobok <tmsukhobok@edu.hse.ru>
2025-07-31 18:05:11 +0300

    fix script3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Для развертывания на GitHub Pages, замените 'ВАШЕ_ИМЯ_РЕПОЗИТОРИЯ'
-  // на имя вашего репозитория (например, '/my-resume-project/').
+
   base: '/front-end-entrance-exam/',
   plugins: [],
 });
